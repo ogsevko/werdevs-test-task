9d495377a984de1d7e6204ce4944852dd74e2ef4
Yaroslav Andriyenko <andriyenko.yaroslav@gmail.com>
2020-10-07 14:33:49 +0300

    Header was added

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export function Header() {
+  return (
+    <header className="header">
+      <div className="header__container">
+        <div className="header__logo">
+          <img
+            src={process.env.PUBLIC_URL + 'logo.svg'}
+            alt="Company logo"
+            className="header__image"
+          />
+        </div>
+        <nav className="header__nav nav">
+          <a
+            href="/#"
+            className="nav__item"
+          >
+            Home
+          </a>
+          <a
+            href="/#"
+            className="nav__item"
+          >
+            About us
+          </a>
+        </nav>
+      </div>
+    </header>
+  )
+}
